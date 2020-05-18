06070a4bd81743aee62d226020d7fc5ff547e52d
root <root@localhost.localdomain>
2020-05-18 18:46:32 +0800

    udate etcd backup

--- etcd/etcd-backup.sh
+++ etcd/etcd-backup.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-etcd_cert_dir=/etc/kubernetes/pki/etcd
-etcd_backup_dir=/var/lib/docker/backup/etcd
-[ -d ${etcd_backup_dir} ] || mkdir -p ${etcd_backup_dir} && mkdir -p ${etcd_backup_dir}/kubernetes
+backup_dir=/data/backup/kubernetes
+etcd_backup_dir=${backup_dir}/etcd
+kubernetes_backup_dir=${backup_dir}/kubernetes
+kubelet_backup_dir=${backup_dir}/kubelet
 
+[ -d ${etcd_backup_dir} ] || mkdir -p ${etcd_backup_dir}
+[ -d ${kubernetes_backup_dir} ] || mkdir -p ${kubernetes_backup_dir}
+[ -d ${kubelet_backup_dir} ] || mkdir -p ${kubelet_backup_dir}
 
 #备份etcd库
 ETCDCTL_API=3
@@ -17,9 +21,13 @@ filelist=`ls /etc/kubernetes |grep -v backup`
 cd /etc/kubernetes
 for file in $filelist
 do
-   cp -r $file ${etcd_backup_dir}/kubernetes/
+   cp -r $file ${kubernetes_backup_dir}/
 done
-cp -r /var/lib/kubelet ${etcd_backup_dir}/
+
+#备份/var/lib/kubelet
+cp -r /var/lib/kubelet/* ${kubelet_backup_dir}/
 
 # 备份保留30天
 find $etcd_backup_dir/ -name *.db -mtime +30 -exec rm -f {} \;
+
+ls ${etcd_backup_dir} 
--- etcd/etcd-restore.sh
+++ etcd/etcd-restore.sh
@@ -1,20 +1,38 @@
 #!/bin/bash
 #by chinaboy007 
 
+
+TIME=`date +%Y%m%d%H%M%S`
 CURRENT_HOST_IP=192.168.227.130
-etcd_backup_dir=/etc/kubernetes/backup/etcd
+backup_dir=/data/backup/kubernetes
+etcd_backup_dir=${backup_dir}/etcd
+etcd_db_name=$1
 
-mv /var/lib/etcd /var/lib/etcd.bak
-mv /var/lib/kubelet /var/lib/kubelet.bak
+cd ${etcd_backup_dir};ls
+
+if [ $# -ne 1 ];then
+   echo "usage:$0 {etcd_db_name}"
+   echo "eg: $0etcd-snapshot-20200515T030001.db"
+   exit 1
+fi
 
 
- ETCDCTL_API=3 etcdctl snapshot restore ${etcd_backup_dir}/etcd-snapshot-20200515T173218.db \
+mv /var/lib/etcd /var/lib/etcd-${TIME}.bak
+mv /etc/kubernetes/manifests /etc/kubernetes/manifests.bak
+sleep 8
+echo "please wait"
+
+ ETCDCTL_API=3 etcdctl snapshot restore ${etcd_backup_dir}/${etcd_db_name} \
   --name etcd-0 \
   --initial-cluster="etcd-0=https://${CURRENT_HOST_IP}:2380" \
   --initial-cluster-token=etcd-cluster-token \
   --initial-advertise-peer-urls=https://${CURRENT_HOST_IP}:2380 \
   --data-dir=/var/lib/etcd
 
-cp -r ${etcd_backup_dir}/kubelet /var/lib/ 
+#cp -r ${backup_dir}/kubelet /var/lib/ 
+#cp -r ${backup_dir}/kubernetes /etc/
 
+mv /etc/kubernetes/manifests.bak /etc/kubernetes/manifests
 systemctl restart kubelet
+systemctl status kubelet
+kubectl get node
